85d6f75809cb10cda18e723f9ebbad566698326c
KevinMath1 <kevinmathias21@gmail.com>
2024-06-06 10:00:54 -0300

    alteração

--- src/app.js
+++ src/app.js
@@ -8,7 +8,8 @@ servidor.use(cors());
 servidor.use(express.json());
 servidor.use(EaglesController);
 
-const port = process.env.PORTA || 3000;
+const port = process.env.PORTA || 5020;
+console.log("Porta configurada: ", port);
 
 // Configurar o servidor para escutar em todas as interfaces de rede (0.0.0.0)
 servidor.listen(port, '0.0.0.0', () => {
